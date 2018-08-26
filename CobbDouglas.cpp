67afc31206bb373fb6812f509a0f2b61aa237662
Bryan Weber <Bryan.S.Weber@gmail.com>
2018-08-25 20:23:23 -0400

    Now tracks enemy stock inside CD model.

--- CobbDouglas.cpp
+++ CobbDouglas.cpp
@@ -103,12 +103,16 @@ bool CobbDouglas::tech_starved()
     }
 }
 
-void CobbDouglas::estimateCD(int e_army_stock, int e_tech_stock, int e_worker_stock)
+void CobbDouglas::estimateCD(int e_army_stock, int e_tech_stock, int e_worker_stock) // FOR MODELING ENEMIES ONLY
 {
     double K_over_L = (double)(e_army_stock + 1) / (double)(e_worker_stock + 1); // avoid NAN's
     alpha_army = CUNYAIModule::bindBetween(K_over_L / (double)(1.0 + K_over_L), 0.05, 0.95);
     alpha_econ = CUNYAIModule::bindBetween(1 - alpha_econ, 0.05, 0.95);
     alpha_tech = CUNYAIModule::bindBetween(e_tech_stock / (double)e_worker_stock * alpha_econ / alpha_army, 0.05, 0.95 );
+
+    army_stock = e_army_stock;
+    tech_stock = e_tech_stock;
+    worker_stock = e_worker_stock;
 }
 
 //Sets enemy utility function parameters based on known information.
