a61c71963a2a27e210e68dcbbdcba9006e6390e6
Bryan Weber <Bryan.S.Weber@gmail.com>
2019-01-24 16:19:28 -0500

    Fixed a bug in map selection and a potential crash in the reservation map

--- Map_Inventory.cpp
+++ Map_Inventory.cpp
@@ -1370,16 +1370,19 @@ void Map_Inventory::updateBasePositions(Unit_Inventory &ui, Unit_Inventory &ei, 
     }
 
     if (frames_since_enemy_base_ground_ > 24 * 10) {
-        checked_all_expo_positions_ = false;
 
-        Stored_Unit* center_building = CUNYAIModule::getClosestGroundStored(ei, ui.getMeanLocation(), *this); // If the mean location is over water, nothing will be updated. Current problem: Will not update if on
+        Stored_Unit* center_army = CUNYAIModule::getClosestGroundStored(ei, ui.getMeanLocation(), *this); // If the mean location is over water, nothing will be updated. Current problem: Will not update if no combat forces!
+        Stored_Unit* center_base = CUNYAIModule::getClosestStoredBuilding(ei, ui.getMeanLocation(),999999); // 
 
-        if (ei.getMeanBuildingLocation() != Positions::Origin && center_building && center_building->pos_ && center_building->pos_ != Positions::Origin) { // Sometimes buildings get invalid positions. Unclear why. Then we need to use a more traditioanl method.
-            updateMapVeinsOut( center_building->pos_, enemy_base_ground_, map_out_from_enemy_ground_, false); // don't print this one, it could be anywhere and to print all of them would end up filling up our hard drive.
+        if (ei.getMeanBuildingLocation() != Positions::Origin && center_army && center_army->pos_ && center_army->pos_ != Positions::Origin) { // Sometimes buildings get invalid positions. Unclear why. Then we need to use a more traditioanl method.
+            updateMapVeinsOut( center_army->pos_, enemy_base_ground_, map_out_from_enemy_ground_, false); // don't print this one, it could be anywhere and to print all of them would end up filling up our hard drive.
+        }
+        else if (ei.getMeanBuildingLocation() != Positions::Origin && center_base && center_base->pos_ && center_base->pos_ != Positions::Origin) { // Sometimes buildings get invalid positions. Unclear why. Then we need to use a more traditioanl method.
+            updateMapVeinsOut(center_base->pos_, enemy_base_ground_, map_out_from_enemy_ground_, false); // don't print this one, it could be anywhere and to print all of them would end up filling up our hard drive.
         }
         else if (!start_positions_.empty() && start_positions_[0] && start_positions_[0] !=  Positions::Origin && !cleared_all_start_positions_) { // maybe it's an starting base we havent' seen yet?
             int attempts = 0;
-            while (Broodwar->isVisible(TilePosition(enemy_base_ground_)) && attempts < static_cast<int>(start_positions_.size()) ) {
+            while (Broodwar->isVisible(TilePosition(enemy_base_ground_)) && attempts < static_cast<int>(start_positions_.size()) && Broodwar->isVisible(TilePosition(start_positions_[0]))) {
                 std::rotate(start_positions_.begin(), start_positions_.begin() + 1, start_positions_.end());
                 attempts++;
             }
@@ -1387,18 +1390,19 @@ void Map_Inventory::updateBasePositions(Unit_Inventory &ui, Unit_Inventory &ei, 
         }
         else if (!expo_positions_complete_.empty()) { // maybe it's a expansion we havent' seen yet?
             expo_positions_ = expo_positions_complete_;
-            int random_index = rand() % expo_positions_.size(); // random enough for our purposes.
-            checked_all_expo_positions_ = true;
-            expo_positions_ = expo_positions_complete_;
-            while (Broodwar->isVisible(TilePosition(enemy_base_ground_)) && !expo_positions_.empty() && Broodwar->isVisible(expo_positions_[random_index])) { // Check any expo we're not looking at.
-                random_index = rand() % expo_positions_.size();
-                if (Broodwar->isVisible(expo_positions_[random_index])) {
-                    expo_positions_.erase(expo_positions_.begin() + random_index);
-                }
+
+            int attempts = 0;
+            while (Broodwar->isVisible(TilePosition(enemy_base_ground_)) && attempts < static_cast<int>(expo_positions_.size()) && Broodwar->isVisible(TilePosition(start_positions_[0]))) {
+                std::rotate(expo_positions_.begin(), expo_positions_.begin() + 1, expo_positions_.end());
+                attempts++;
             }
 
-            if (!expo_positions_.empty()) {
+            if (attempts >= static_cast<int>(expo_positions_.size())) {
+                int random_index = rand() % static_cast<int>(expo_positions_.size() - 1); // random enough for our purposes.
                 updateMapVeinsOut(Position(expo_positions_[random_index]) + Position(UnitTypes::Zerg_Hatchery.dimensionLeft(), UnitTypes::Zerg_Hatchery.dimensionUp()) , enemy_base_ground_, map_out_from_enemy_ground_);
+            }
+            else { // you can see everything but they have no enemy forces, then let's go smash randomly.
+                updateMapVeinsOut(Position(expo_positions_[0]) + Position(UnitTypes::Zerg_Hatchery.dimensionLeft(), UnitTypes::Zerg_Hatchery.dimensionUp()), enemy_base_ground_, map_out_from_enemy_ground_);
             }
         }
         frames_since_enemy_base_ground_ = 0;
