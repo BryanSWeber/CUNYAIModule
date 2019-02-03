7eda414e02fc3608c618886c0e8c952e0555c94c
Bryan Weber <Bryan.S.Weber@gmail.com>
2019-02-03 00:12:13 -0500

    Update CUNYAIModule.cpp
    Can't use all of 'em or it'll crash pretty hard. Too much CPU usage

--- Source/CUNYAIModule.cpp
+++ Source/CUNYAIModule.cpp
@@ -401,8 +401,8 @@ void CUNYAIModule::onFrame()
     vector<vector<int>> pf_aa = current_map_inventory.createEmptyField();
     vector<vector<int>> pf_explore = current_map_inventory.createEmptyField();
 
-    current_map_inventory.createThreatField(pf_threat, enemy_player_model);
-    current_map_inventory.createAttractField(pf_attract, enemy_player_model);
+    //current_map_inventory.createThreatField(pf_threat, enemy_player_model);
+    //current_map_inventory.createAttractField(pf_attract, enemy_player_model);
     current_map_inventory.createAAField(pf_aa, enemy_player_model);
     current_map_inventory.createExploreField(pf_explore);
 
