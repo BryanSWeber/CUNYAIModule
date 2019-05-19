e18857a5b4e82acbd393b320e6778bdafd019f71
Bryan Weber <Bryan.S.Weber@gmail.com>
2019-05-19 14:01:02 -0400

    Create CombatManager.h

---
+++ CombatManager.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Source\CUNYAIModule.h"
+#include "Source\Unit_Inventory.h"
+#include <bwem.h>
+
+class CombatManager {
+    private:
+        Unit_Inventory anti_air_squad_;
+        Unit_Inventory anti_ground_squad_;
+        Unit_Inventory universal_squad_;
+        Unit_Inventory liabilities_squad_;
+        Unit_Inventory scout_squad_;
+    public:
+
+};
