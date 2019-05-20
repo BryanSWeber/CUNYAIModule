d67d4a96f6c61504d54e716b95b6cba5431f1647
Bryan Weber <Bryan.S.Weber@gmail.com>
2019-05-20 10:11:41 -0400

    Working on Combat Manager

--- CombatManager.h
+++ CombatManager.h
@@ -6,11 +6,19 @@
 
 class CombatManager {
     private:
+        //Squad only attacks up.
         Unit_Inventory anti_air_squad_;
+        //Squad only attacks down.
         Unit_Inventory anti_ground_squad_;
+        //Squad attacks either.
         Unit_Inventory universal_squad_;
+        //Squad only gets hurt. Overlords, low energy psions...
         Unit_Inventory liabilities_squad_;
+        //Squad scouts
         Unit_Inventory scout_squad_;
+        //Squad detects
+        Unit_Inventory detector_squad_;
+
     public:
         bool identifyTargets();
         bool identifyWeaknesses();
