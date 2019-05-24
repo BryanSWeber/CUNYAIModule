0d124c79f541268eff8bbe1a4214f2b6bd7ffad4
Bryan Weber <Bryan.S.Weber@gmail.com>
2019-05-24 09:20:06 -0400

    A possible tech bug resolved.

--- Source/TechManager.h
+++ Source/TechManager.h
@@ -14,7 +14,7 @@ public:
     // Checks if a research can be built, and passes additional boolean critera, if all criteria are passed, then it performs the research. 
     static bool Check_N_Research(const TechType & tech, const Unit & unit, const bool & extra_critera);
     static void Print_Upgrade_FAP_Cycle(const int & screen_x, const int & screen_y);
-    static void updateTech_Avail();
+    static bool updateTech_Avail();
     static void updateOptimalTech();
     static bool checkTechAvail();
     static bool checkBuildingReady(const UpgradeType up);
