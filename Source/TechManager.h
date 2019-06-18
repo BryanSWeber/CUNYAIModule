45e545d8f28f2ec781fd136515e1084e76237f53
Bryan Weber <Bryan.S.Weber@gmail.com>
2019-06-18 16:43:50 -0400

    Update TechManager.h

--- Source/TechManager.h
+++ Source/TechManager.h
@@ -17,8 +17,8 @@ public:
     static bool Check_N_Research(const TechType & tech, const Unit & unit, const bool & extra_critera);
     static void Print_Upgrade_FAP_Cycle(const int & screen_x, const int & screen_y);
     static bool updateTech_Avail();
-    void updateOptimalTech();
-    void updateMaxGas();
+    static void updateOptimalTech();
+    static void updateMaxGas();
     // Return True if there's something I might want to make that is a tech building.
     static bool checkTechAvail();
     // Check if building is made and complete.
@@ -29,7 +29,7 @@ public:
     static bool checkBuildingReady(const TechType tech);
     // Check if I have a unit that could use this upgrade, ex melee damage with only hydras.
     static bool checkUpgradeUseable(const UpgradeType up);
-    bool Tech_BeginBuildFAP(Unit building, Unit_Inventory &ui, const Map_Inventory &inv);
+    static bool Tech_BeginBuildFAP(Unit building, Unit_Inventory &ui, const Map_Inventory &inv);
     static void clearSimulationHistory(); //Clears the MA history. Should run every time a relevant comabat unit is made/destroyed to prevent the MA from having weight in dissimilar situations.
     static int returnTechRank(const UpgradeType & ut);
     // Returns the most expensive piece of Tech we've considered making.
